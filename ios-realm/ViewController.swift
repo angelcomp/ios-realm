e8ee20d89a93dc601e89be6c92b73f96a839a906
Angelica dos Santos <santosangelicassp@gmail.com>
2022-11-17 13:00:09 -0300

    save data

--- ios-realm/ViewController.swift
+++ ios-realm/ViewController.swift
@@ -15,7 +15,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        save(firstName: "Angelica", lastName: "Santos", age: 22)
+        save(firstName: "Maria", lastName: "Silva", age: 36)
+        save(firstName: "José", lastName: "Oliveira", age: 27)
+    }
+    
+    private func save(firstName: String, lastName: String, age: Int) {
+        let person = Person()
+        person.firstName = firstName
+        person.lastName = lastName
+        person.age = age
         
+        realm.beginWrite()
+        realm.add(person)
+        try! realm.commitWrite()
     }
 }
 
